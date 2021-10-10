b5333d830e818ff5d9e2ad11b048a9d54f060f53
cecyRdz <92197205+cecyRdz@users.noreply.github.com>
2021-10-10 18:49:19 -0500

    Creación de las carpetas para organizar el código
    Creación de el archivo app en directorio controlador
    Creación de la carpeta vista
    Creación del Index.html

---
+++ controlador/app.py
@@ -0,0 +1,9 @@
+from flask import Flask,render_template, request
+app = Flask(__name__, template_folder='../vista')
+
+@app.route('/')
+def inicio():
+    return render_template('comunes/Index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
