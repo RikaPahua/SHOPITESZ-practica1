e5c07282b9bcc0c880e02055e02b6924a4d06755
MartinVaca <mvaca413@accitesz.com>
2021-11-15 16:06:16 -0600

    Aplicación de estilos
    ...

--- controlador/app.py
+++ controlador/app.py
@@ -1,5 +1,8 @@
 from flask import Flask,render_template, request
-app = Flask(__name__, template_folder='../vista')
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__, template_folder='../vista', static_folder='../static')
+Bootstrap(app)
 
 @app.route('/')
 def inicio():
